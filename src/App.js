b56aec697e792e01a52bc18be2dc3036277b34d1
Dean Thomson <dean.thomson@derivco.co.za>
2021-05-16 16:42:40 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+const HatsPage = () => <h1>HATS</h1>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
