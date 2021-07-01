24acf251c42c838abc457d59c45e9b98eef5c5f8
Dean Thomson <dean.thomson@derivco.co.za>
2021-07-01 11:28:44 +0200

    implemented firebase utils, included ability to store authenticated users into firebase db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,10 +11,37 @@ const config = {
   appId: '1:447108409848:web:ec6d9881afd7b06fd0ce0d'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.id}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+firestore.enablePersistence();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
